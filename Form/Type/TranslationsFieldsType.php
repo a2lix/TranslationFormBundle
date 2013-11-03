52e5fab6151f724aaff8cf4e33f7ea909b7030df
daniel.pinciuc <daniel.pinciuc>
2013-11-03 13:51:48 +0200

    Change a2lix_translations form type configuration type index into form_type

--- Form/Type/TranslationsFieldsType.php
+++ Form/Type/TranslationsFieldsType.php
@@ -16,8 +16,8 @@ class TranslationsFieldsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         foreach ($options['fields'] as $fieldName => $fieldConfig) {
-            $fieldType = $fieldConfig['type'];
-            unset($fieldConfig['type']);
+            $fieldType = $fieldConfig['form_type'];
+            unset($fieldConfig['form_type']);
 
             $builder->add($fieldName, $fieldType, $fieldConfig);
         }
