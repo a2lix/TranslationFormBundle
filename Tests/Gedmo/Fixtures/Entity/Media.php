8a2c565fd788c2cced524f6aedb9e0cf46fb2fe6
webda2l <webda2l@gmail.com>
2013-11-10 13:42:19 +0100

    Start adding some tests

---
+++ Tests/Gedmo/Fixtures/Entity/Media.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace A2lix\TranslationFormBundle\Tests\Gedmo\Fixtures\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity
+ */
+class Media
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    
+    /**
+     * @ORM\Column(length=10)
+     */
+    protected $locale;
+
+    /**
+     * @var Product $product
+     * @ORM\ManyToOne(targetEntity="Product", inversedBy="medias")
+     */
+    protected $product;
+
+    /**
+     * @ORM\Column(nullable=true)
+     */
+    protected $url;
+
+    /**
+     * @ORM\Column(nullable=true)
+     */
+    protected $description;
+    
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getLocale()
+    {
+        return $this->locale;
+    }
+
+    public function setLocale($locale)
+    {
+        $this->locale = $locale;
+        return $this;
+    }
+    
+    public function getProduct()
+    {
+        return $this->product;
+    }
+
+    public function setProduct(Product $product)
+    {
+        $this->product = $product;
+    }
+
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    public function setUrl($url)
+    {
+        $this->url = $url;
+        return $this;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+}
---
+++ Tests/Gedmo/Fixtures/Entity/Product.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace A2lix\TranslationFormBundle\Tests\Gedmo\Fixtures\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * @ORM\Entity
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(nullable=true)
+     * @Gedmo\Translatable
+     */
+    protected $title;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     * @Gedmo\Translatable
+     */
+    protected $description;
+
+    /**
+     * @ORM\Column(nullable=true)
+     */
+    protected $url;
+    
+    /**
+     * @var \Doctrine\Common\Collections\ArrayCollection $medias
+     * @ORM\OneToMany(targetEntity="Media", mappedBy="product", indexBy="locale", cascade={"all"}, orphanRemoval=true)
+     * @Assert\Valid
+     */
+    protected $medias;
+    
+    /**
+     * @ORM\OneToMany(targetEntity="ProductTranslation", mappedBy="object", indexBy="locale", cascade={"all"})
+     * @Assert\Valid
+     */
+    protected $translations;
+
+    public function __construct()
+    {
+        $this->medias = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->translations = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+    
+    public function setTitle($title)
+    {
+        $this->title = $title;
+        return $this;
+    }
+    
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    public function setUrl($url)
+    {
+        $this->url = $url;
+        return $this;
+    }
+    
+    public function getTranslations()
+    {
+        return $this->translations;
+    }
+
+    public function addTranslation(ProductTranslation $translation)
+    {
+        if (!$this->translations->contains($translation)) {
+            $translation->setObject($this);
+            $this->translations->set($translation->getLocale(), $translation);
+        }
+        return $this;
+    }
+
+    public function removeTranslation(ProductTranslation $translation)
+    {
+        if ($this->translations->contains($translation)) {
+            $this->translations->removeElement($translation);
+        }
+        return $this;
+    }
+    
+    public function getMedias()
+    {
+        return $this->medias;
+    }
+
+    public function addMedia(Media $media)
+    {
+        if (!$this->medias->contains($media)) {
+            $media->setProduct($this);
+            $this->medias->set($media->getLocale(), $media);
+        }
+        return $this;
+    }
+
+    public function removeMedia(Media $media)
+    {
+        if ($this->medias->contains($media)) {
+            $this->medias->removeElement($media);
+        }
+        return $this;
+    }
+}
