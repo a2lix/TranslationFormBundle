968fad9514d1cc532fefc55cf638fe02ac11f6e1
webda2l <webda2l@gmail.com>
2013-12-15 18:33:41 +0100

    New a2lix_translatedEntity field. fix #94

---
+++ Form/Type/TranslatedEntityType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace A2lix\TranslationFormBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType,
+    Symfony\Component\OptionsResolver\OptionsResolverInterface,
+    Symfony\Component\OptionsResolver\Options,
+    Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Translated entity
+ *
+ * @author David ALLIX
+ */
+class TranslatedEntityType extends AbstractType
+{
+    private $request;
+    
+    public function setRequest(Request $request = null)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * 
+     * @param \Symfony\Component\OptionsResolver\OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'translation_path' => 'translations',
+            'translation_property' => null,
+            'property' => function(Options $options) {
+                if (null === $this->request) {
+                    throw new \Exception('Error while getting request');
+                }
+                
+                return $options['translation_path'] .'['. $this->request->getLocale() .'].'. $options['translation_property'];
+            },
+        ));
+    }
+
+    public function getParent()
+    {
+        return 'entity';
+    }
+    
+    public function getName()
+    {
+        return 'a2lix_translatedEntity';
+    }
+}
