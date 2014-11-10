1e18772d193d23bafbf4bdfe3b59f99ff7fdfc78
David ALLIX <webda2l@gmail.com>
2014-11-10 16:00:41 +0100

    // Manage focus on right bootstrap tab when invalid event (A2lixTranslation tab or not, and inner tabs include)

--- Resources/public/js/a2lix_translation_bootstrap.js
+++ Resources/public/js/a2lix_translation_bootstrap.js
@@ -13,4 +13,37 @@ $(function() {
 
         $('ul.a2lix_translationsLocales li:visible:first').find('a').tab('show');
     }).trigger('change');
+
+    // Manage focus on right bootstrap tab when invalid event (A2lixTranslation tab or not, and inner tabs include)
+    $(':input', 'div.tab-content').on('invalid', function(e) {
+        var $tabPanes = $(this).parents('div.tab-pane');
+
+        $tabPanes.each(function() {
+            var $tabPane = $(this);
+
+            if (!$tabPane.hasClass('active')) {
+                var $tabNavs = $tabPane.parent('.tab-content')
+                                       .siblings('ul.nav.nav-tabs');
+
+                // Tab target by id
+                if (this.id) {
+                    $tabNavs.find('a[href="#'+ this.id +'"], a[data-target="#'+ this.id +'"]')
+                            .trigger('click');
+
+                    return true;
+                }
+
+                // Tab target by class for a2lixTranslation
+                var a2lixTranslClass = /a2lix_translationsFields-[\S]+/.exec(this.className);
+                if (a2lixTranslClass.length) {
+                    $tabNavs.find('a[data-target=".'+ a2lixTranslClass[0] +'"]')
+                            .trigger('click');
+
+                    return true;
+                }
+            }
+        });
+
+        return true;
+    });
 });
