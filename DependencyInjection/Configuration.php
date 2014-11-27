493c4f3d5bea4af5747a2ab3cec5d1835f5bb9ed
Richard van Laak <rvanlaak@gmail.com>
2014-11-27 16:34:17 +0100

    Enable TranslationFormBundle specific default_locale
    ... so it also can be used in case the Symfony framework `default_locale` cannot be used.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -22,6 +22,7 @@ class Configuration implements ConfigurationInterface
         $rootNode
             ->children()
                 ->scalarNode('locale_provider')->defaultValue('default')->end()
+                ->scalarNode('default_locale')->isRequired(false)->end()
                 ->arrayNode('locales')
                     ->beforeNormalization()
                         ->ifString()
