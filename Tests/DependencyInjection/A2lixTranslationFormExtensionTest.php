e84d7259adba88f4a85d0d729d99ab0c874ea8b3
Jordi Sala Morales <jordism91@gmail.com>
2018-03-11 17:59:15 +0100

    Add dependency injection tests

---
+++ Tests/DependencyInjection/A2lixTranslationFormExtensionTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the TranslationFormBundle package.
+ *
+ * (c) David ALLIX <http://a2lix.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace A2lix\TranslationFormBundle\Tests\DependencyInjection;
+
+use A2lix\TranslationFormBundle\DependencyInjection\A2lixTranslationFormExtension;
+use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractExtensionTestCase;
+
+class A2lixTranslationFormExtensionTest extends AbstractExtensionTestCase
+{
+    public function testAfterLoadingParametersAreSet()
+    {
+        $this->load();
+        $this->assertContainerBuilderHasParameter('a2lix_translation_form.locale_provider', 'default');
+        $this->assertContainerBuilderHasParameter('a2lix_translation_form.locales', array('es', 'en'));
+        $this->assertContainerBuilderHasParameter('a2lix_translation_form.required_locales', array());
+        $this->assertContainerBuilderHasParameter('a2lix_translation_form.default_locale', 'es');
+        $this->assertContainerBuilderHasParameter(
+            'a2lix_translation_form.templating',
+            'A2lixTranslationFormBundle::default.html.twig'
+        );
+        $this->assertContainerBuilderHasAlias('a2lix_translation_form.manager_registry', 'doctrine');
+    }
+
+    protected function getContainerExtensions()
+    {
+        return array(
+            new A2lixTranslationFormExtension(),
+        );
+    }
+
+    protected function getMinimalConfiguration()
+    {
+        return array(
+            'locales' => array('es', 'en'),
+            'default_locale' => 'es',
+        );
+    }
+}
