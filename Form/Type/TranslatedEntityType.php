333474659f1b89bb89810a1de5f7303f8eaf2246
Jordi Sala Morales <jordism91@gmail.com>
2018-03-12 22:41:18 +0100

    Fix parent form type on SF3

--- Form/Type/TranslatedEntityType.php
+++ Form/Type/TranslatedEntityType.php
@@ -63,7 +63,10 @@ class TranslatedEntityType extends AbstractType
 
     public function getParent()
     {
-        return 'entity';
+        return
+            method_exists('Symfony\Component\Form\AbstractType', 'getBlockPrefix') ?
+            'Symfony\Bridge\Doctrine\Form\Type\EntityType' :
+            'entity';
     }
 
     // BC for SF < 3.0
